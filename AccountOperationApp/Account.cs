9275272af5da2051237cda52e69c7a40a585445f
baridcse <baridcse@gmail.com>
2015-05-02 16:25:43 +0600

    Account Operation

---
+++ AccountOperationApp/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOperationApp
+{
+    class Account
+    {
+        public string AccountNumber;
+        public string AccountName;
+        public double AccountBalance;
+
+        public string Deposit(double amount)
+        {
+            if (AccountName!=null && AccountNumber!=null)
+            {
+                AccountBalance += amount;
+                return "Deposit Successfully Done";    
+            }
+            else
+            {
+                return "Please Create Account ";
+            }
+            
+        }
+
+        public string Withdraw(double amount)
+        {
+            if (AccountName != null && AccountNumber != null)
+            {
+                if (AccountBalance >= amount)
+                {
+                    AccountBalance -= amount;
+                    return "Withdraw Successfully Done";
+                }
+                else
+                {
+                    return "You Have not Enough Balance";
+                } 
+            }
+            else
+            {
+                return "Please Create Account ";
+            }
+        }
+    }
+}
---
+++ AccountOperationApp/AccountOperationUI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationApp
+{
+    public partial class AccountOperationUI : Form
+    {
+        Account anAccount = new Account();
+        public AccountOperationUI()
+        {
+            InitializeComponent();
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            anAccount.AccountNumber = accountNumberTextBox.Text;
+            anAccount.AccountName = customerNameTextBox.Text;
+            MessageBox.Show("Account Created");
+            accountNumberTextBox.Clear();
+            customerNameTextBox.Clear();
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            double amount = Convert.ToDouble(amountTextBox.Text);
+            string message = anAccount.Deposit(amount);
+            MessageBox.Show(message);
+            amountTextBox.Clear();
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            double amount = Convert.ToDouble(amountTextBox.Text);
+            string message = anAccount.Withdraw(amount);
+            MessageBox.Show(message);
+            amountTextBox.Clear();
+        }
+
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(anAccount.AccountName + ", Your Account Number is: " + anAccount.AccountNumber +
+                            " and it's Balance: " + anAccount.AccountBalance+" Taka");
+        }
+    }
+}
